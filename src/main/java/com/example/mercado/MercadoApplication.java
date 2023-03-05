97b3e4223c5787ff2193a38499997f42c26b3cea
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-05 12:05:28 -0300

    upload de queue e MercadoApplication

--- src/main/java/com/example/mercado/MercadoApplication.java
+++ src/main/java/com/example/mercado/MercadoApplication.java
@@ -1,9 +1,18 @@
 package com.example.mercado;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Mercado api", version = "1.0", description = "Mercado API"))
+@EnableKafka
+@EnableRabbit
 public class MercadoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/mercado/fornecimento/queue/out/PagarFornecedorMessageSender.java
@@ -0,0 +1,34 @@
+package com.example.mercado.fornecimento.queue.out;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+import com.example.mercado.fornecimento.client.payload.request.RealizarPagamentoFinanceiro;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PagarFornecedorMessageSender {
+    private final RabbitTemplate rabbitTemplate;
+
+    private final Queue queue;
+
+    private final ObjectMapper objectMapper;
+
+    public void send(RealizarPagamentoFinanceiro realizarPagamentoFinanceiro) {
+        String message = null;
+        try {
+            message = objectMapper.writeValueAsString(realizarPagamentoFinanceiro);
+            log.info("Mensagem enviada para o Rabbit {} {}",
+                    realizarPagamentoFinanceiro.getItems(), realizarPagamentoFinanceiro.getFornecedor());
+            rabbitTemplate.convertSendAndReceive(queue.getName(),message);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
