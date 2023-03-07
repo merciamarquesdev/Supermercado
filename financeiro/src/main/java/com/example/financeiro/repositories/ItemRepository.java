e62a9a876042385e91fe0e75cb494195bc288082
Ana Thais Senger <anathaissenger@hotmail.com>
2023-03-07 19:46:54 -0300

    arrumando os modulos

--- financeiro/src/main/java/com/example/financeiro/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.financeiro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.financeiro.entity.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
